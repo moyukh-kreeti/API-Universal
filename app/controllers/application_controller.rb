58a1948de3792e4d82e26c5225e14b0b0a3cdbcb
moyukh-kreeti <moyukh.sarkar@kreeti.com>
2023-12-26 00:39:41 +0530

    Authentication and User crud api documentation done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,10 @@ class ApplicationController < ActionController::Base
 
   def verify_authorization
     header = request.headers['authorization']
-    token  = header.split(" ")[1]
-    @auth_data  = JwtWebToken.decode(token)
+    if(header)
+      token  = header.split(" ")[1]
+      @auth_data  = JwtWebToken.decode(token)
+    end
     unless @auth_data
       render :json => {msg: "Session Expired"},status: :unauthorized
     end
