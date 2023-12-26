6c29417d3fa7b3e574d193f6e875d7ab5cd47e0a
Chitram Dasgupta <chitram.dasgupta@kreeti.com>
2023-12-26 21:28:14 +0530

    Added Third-Party Integration

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,39 @@
+class WordsController < ApplicationController
+  skip_before_action :verify_authorization, only: %i[meaning synonym]
+
+  # The API endpoint will be of the form:
+  # words/meaning?word=<our_word>
+  def meaning
+    api_url = "https://api.dictionaryapi.dev/api/v2/entries/en/#{params['word']}"
+    response = HTTParty.get(api_url, headers: { 'Content-Type' => 'application/json' })
+
+    return render json: { 'message': 'Word not found' }, status: :not_found unless response.code == 200
+
+    meanings = []
+    response.parsed_response[0]['meanings'].each do |meaning|
+      meanings.append(meaning['definitions'][0]['definition'])
+    end
+
+    render json: meanings
+  end
+
+  # The API endpoint will be of the form:
+  # words/synonym?word=<our_word>
+  def synonym
+    api_url = "https://api.dictionaryapi.dev/api/v2/entries/en/#{params['word']}"
+    response = HTTParty.get(api_url, headers: { 'Content-Type' => 'application/json' })
+
+    return render json: { 'message': 'Word not found' }, status: :not_found unless response.code == 200
+
+    synonyms = []
+    response.parsed_response[0]['meanings'].each do |meaning|
+      next if meaning['synonyms'].empty?
+
+      meaning['synonyms'].each do |synonym|
+        synonyms.append(synonym)
+      end
+    end
+
+    synonyms.empty? ? (render json: { message: 'No synonyms found' }) : (render json: synonyms)
+  end
+end
