521a559f598defaf66541a661f41a59491b7750f
Gaurav Pareek <gaurav.pareek@kreeti.com>
2023-12-26 12:33:55 +0530

    users controller test added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, if: -> { request.format.json? }
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_method
   before_action :verify_authorization
 
   def verify_authorization
     header = request.headers['authorization']
-    token  = header.split(" ")[1]
-    @auth_data  = JwtWebToken.decode(token)
+    token  = header ? header.split(" ")[1] : nil
+    @auth_data  = token ? JwtWebToken.decode(token) : nil
     unless @auth_data
       render :json => {msg: "Session Expired"},status: :unauthorized
     end
+  end
+
+  def not_found_method 
+    render :json => {msg: "record not found "},status: :not_found
   end
 end
