aa820eec05d1d9fdad65f8c8e245324be76ebe49
singh-azad <azad.singh@kreeti.com>
2023-12-29 15:14:30 +0530

    added document destroy action

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 User.create!(first_name: 'Moyukh', last_name: 'TCS', email: 'moyukh@gmail.com', password: 'password' )
-User.create!(first_name: 'Sumitava', last_name: 'TCS', email: 'moyukh@gmail.com', password: 'password' )
-User.create!(first_name: 'ram', last_name: 'TCS', email: 'moyukh@gmail.com', password: 'password' )
-User.create!(first_name: 'gaurav', last_name: 'TCS', email: 'moyukh@gmail.com', password: 'password' )
-User.create!(first_name: 'lokesh', last_name: 'TCS', email: 'moyukh@gmail.com', password: 'password' )
+User.create!(first_name: 'Sumitava', last_name: 'TCS', email: 'moyukh1@gmail.com', password: 'password' )
+User.create!(first_name: 'ram', last_name: 'TCS', email: 'moyukh2@gmail.com', password: 'password' )
+User.create!(first_name: 'gaurav', last_name: 'TCS', email: 'moyukh3@gmail.com', password: 'password' )
+User.create!(first_name: 'lokesh', last_name: 'TCS', email: 'moyukh4@gmail.com', password: 'password' )
+User.create!(first_name: 'Azad', last_name: 'Singh', email: 'azad@gmail.com', password: 'password' )
